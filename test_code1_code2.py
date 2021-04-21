2a2267dca0bbe2cce0aacbdd724605f8109e1ea8
kuzmin@hotmail.com <kuzmin@hotmail.com>
2021-04-21 21:16:08 +0000

    Testing 2021-04-21 14:16

--- test_code1_code2.py
+++ test_code1_code2.py
@@ -44,8 +44,9 @@ class Test1Fixture(NutterFixture):
 
 result = Test1Fixture().execute_tests()
 print(result.to_string())
-# Comment out the next line (result.exit(dbutils)) to see the test result report from within the notebook
 # Add a comment here for branch4
 is_job = dbutils.notebook.entry_point.getDbutils().notebook().getContext().currentRunId().isDefined()
-if is_job:
-  result.exit(dbutils)
+
+# Comment out the two lines below to see the test result in the notebook
+#if is_job:
+#  result.exit(dbutils)
