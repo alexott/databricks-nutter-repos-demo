fef4e0ff9f63eb5c553231a4787a93cc1ab08207
alexandruanghel <alexandruanghel>
2021-07-23 06:03:54 +0000

    test

--- Code2.py
+++ Code2.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 def generate_data2(table_name="my_data"):
-  df = spark.range(0,10)
+  df = spark.range(0,11)
   df.write.format("delta").mode("overwrite").saveAsTable(table_name)
 
 # COMMAND ----------
