0e2c9e2e0e113f9a5a659b7ff664b02223b6b4aa
alexott <alexott>
2022-01-13 12:13:50 +0000

    Rename fixture

--- unit-tests/test_with_arbitrary_files.py
+++ unit-tests/test_with_arbitrary_files.py
@@ -18,33 +18,33 @@ from runtime.nutterfixture import NutterFixture, tag
 # https://github.com/MrPowers/chispa
 from chispa.dataframe_comparer import *
 
-class Test1Fixture(NutterFixture):
+class TestFixtureArbitraryFiles(NutterFixture):
   def __init__(self):
     self.code2_table_name = "my_data"
     self.code1_view_name = "my_cool_data"
     self.code1_num_entries = 100
     NutterFixture.__init__(self)
     
-  def run_code1(self):
+  def run_code1_arbitrary_files(self):
     generate_data1(spark, n = self.code1_num_entries, name = self.code1_view_name)
     
-  def assertion_code1(self):
+  def assertion_code1_arbitrary_files(self):
     df = spark.read.table(self.code1_view_name)
     assert(df.count() == self.code1_num_entries)
     
-  def run_code2(self):
+  def run_code2_arbitrary_files(self):
     generate_data2(spark, table_name = self.code2_table_name)
     
-  def assertion_code2(self):
+  def assertion_code2_arbitrary_files(self):
     some_tbl = sqlContext.sql(f'SELECT COUNT(*) AS total FROM {self.code2_table_name}')
     first_row = some_tbl.first()
     assert (first_row[0] == 10)
 
-  def after_code2(self):
+  def after_code2_arbitrary_files(self):
     spark.sql(f"drop table {self.code2_table_name}")
     
   # we're using Chispa library here to compare the content of the processed dataframe with expected results
-  def assertion_upper_columns(self):
+  def assertion_upper_columns_arbitrary_files(self):
     cols = ["col1", "col2", "col3"]
     df = spark.createDataFrame([("abc", "cef", 1)], cols)
     upper_df = upper_columns(df, cols)
