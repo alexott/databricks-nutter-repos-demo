2585d066a9422eee16daf7a21c99d967bc6bd82b
rohan-databricks <rohan-databricks>
2022-01-21 22:54:46 +0000

    testing

--- Code1.py
+++ Code1.py
@@ -5,6 +5,7 @@ from pyspark.sql import DataFrame
 
 # COMMAND ----------
 
+#this function creates a dataframe
 def generate_data1(n=1000, name='my_cool_data'):
   df = spark.range(0, n)
   df.createOrReplaceTempView(name)
