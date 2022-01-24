72f5e7d2c3947665c76f5973333eba21b8cd1edf
rohan-databricks <rohan-databricks>
2022-01-24 05:32:28 +0000

    added a comment

--- Code2.py
+++ Code2.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+
+
+# COMMAND ----------
+
+#this creates delta table
 def generate_data2(table_name="my_data"):
   df = spark.range(0,10)
   df.write.format("delta").mode("overwrite").saveAsTable(table_name)
